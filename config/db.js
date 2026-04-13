4a60fca55a6742910dcaf6467252e74165b12cfa
vishalkumar630696 <vishalshakyavanshi38@gmail.com>
2026-04-13 17:48:22 +0530

    update something

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2/promise");
-
+require("dotenv").config();
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Vishal@630696",
-  database: "centraldb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10
 });
 
 // test connection
