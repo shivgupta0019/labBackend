f1564d85574e086b03ed4de57a8577e02ce0cb97
shivgupta0019 <shivgupta0019@gmail.com>
2026-04-14 13:59:49 +0530

    fix db

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
-require("dotenv").config();
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD || "123456",
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
 });
 
 // test connection
