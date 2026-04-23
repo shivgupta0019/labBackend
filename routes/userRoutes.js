72187ffae3fc38a6afcea047ff58a87119688cd6
vishalkumar630696 <vishalshakyavanshi38@gmail.com>
2026-04-23 10:22:27 +0530

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const { signup } = require("../controllers/userController");
+const {
+  login,
+  verifyOtp,
+  resendOtp,
+  forgotPassword,
+  resetPassword,
+  getUsers,
+  updateUserRole,
+  toggleAdmin,
+} = require("../controllers/authController");
 const { login, verifyOtp, resendOtp, forgotPassword, resetPassword, getUsers, updateUserRole, toggleAdmin, logout, } = require("../controllers/authController");
 const authMiddleware = require("../middleware/authMiddleware");
+const {
+  allCompanies,
+  getAllCompanies,
+  updateCompany,
+  deleteCompany,
+  allLabs,
+  getAllLabs,
+  updateLab,
+  deleteLab,
+  allProducts,
+  deleteProduct,
+  getAllProducts,
+  getAllTest,
+  createTest,
+} = require("../controllers/useAdminLab");
 const rateLimit = require("express-rate-limit");
 
 const otpLimiter = rateLimit({
@@ -18,9 +43,29 @@ router.post("/login", login);
 router.post("/verify-otp", verifyOtp);
 router.post("/resend-otp",otpLimiter, resendOtp);
 
-router.post("/forgot-password",forgotPassword);
+router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 
+router.get("/users", getUsers);
+router.put("/users/:id/role", updateUserRole);
+router.post("/toggle-admin", toggleAdmin);
+//lab routes
+router.post("/companies", allCompanies);
+router.get("/getCompanies", getAllCompanies);
+router.put("/companies/:id", updateCompany);
+router.delete("/companies/:id", deleteCompany);
+router.post("/labs", allLabs);
+router.get("/labs", getAllLabs);
+router.put("/labs/:id", updateLab);
+router.delete("/labs/:id", deleteLab);
+router.post("/products", allProducts);
+router.get("/products", getAllProducts);
+router.delete("/products/:id", deleteProduct);
+router.get("/tests", getAllTest);
+router.post("/create-test", createTest);
+// router.get("/dashboard", authMiddleware, (req, res) => {
+//   res.json({ message: "Welcome Vishal" });
+// });
 router.get("/users",authMiddleware,getUsers );
 router.put("/users/:id/role",authMiddleware,updateUserRole);
 router.post("/toggle-admin",authMiddleware, toggleAdmin);
